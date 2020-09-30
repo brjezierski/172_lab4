221e553c820e59078a334463c2bde8f835a6d7b1
Bartek Jezierski <brjezierski@gmail.com>
2020-09-30 13:24:18 -0400

    Change structure, add infromative error detection.

--- Lab4Test-Script.py
+++ Lab4Test-Script.py
@@ -1,7 +1,7 @@
-# Lab2Test-Script.py
-# Created by Kelvin Ferreiras, modified by Vladimir Maksimovski, Dominick Harasimiuk
+# Lab4Test-Script.py
+# Created by Kelvin Ferreiras, modified by Vladimir Maksimovski, Dominick Harasimiuk, Akira
 # Created on Nov. 16, 2017
-# Last Modification on Feb. 16, 2019 by Akira
+# Last Modification on Sep. 30, 2020 by Bartlomiej Jezierski
 # This program tests Lab4 for CSC-172
 
 import subprocess
@@ -15,12 +15,11 @@ dirPath = os.path.dirname(realPath)
 
 labname = 'DNAList' # lab4 only has one main file
 arraysize = '20'
-labTests = ['Lab4Test1','Lab4Test2','Lab4Test3','Lab4Test4','Lab4Test5','Lab4Test6','Lab4Test7','Lab4Test8','Lab4Test9'
-,'Lab4Test10','Lab4Test11','Lab4Test12','Lab4Test13','Lab4Test14','Lab4Test15','Lab4Test16','Lab4Test17']#names of tests (subdirectories of tests folder and names of command text files)
+input_file_name = 'Lab4Test'
 
 # Take the name of all the .zip files into a list
 submissions=glob.glob(dirPath + "/*.zip")
-test_case_directory = '/tests/'
+output_case_directory = '/output/'
 
 out_file_extension = '.out'
 ans_file_extension = '.ans'
@@ -29,13 +28,10 @@ ans_file_extension = '.ans'
 FNULL = open(os.devnull, 'w')
 subprocess.call('rm *.java', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
 subprocess.call('rm *.class', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-subprocess.call('rm tests/Lab4Test1/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-subprocess.call('rm tests/Lab4Test2/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-subprocess.call('rm tests/Lab4Test3/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
 
-#src argument deleted for lab 4 cuz it only has one main file
-def runTestCase(test, test_out, test_ans):
-    subprocess.call('java ' + labname + ' ' + arraysize + ' ' + test + '.txt ' + '>' + '\"' + test_out + '\"', shell=True)
+# Run the test case of the given number
+def runTestCase(test_num, test_out, test_ans):
+    subprocess.call('java ' + labname + ' ' + arraysize + ' input/' + input_file_name + test_num + '.txt ' + '>' + '\"' + test_out + '\"', shell=True)
 
     # Compare compressed and the decompressed output file with the original file
     compare_command = 'diff -w -B ' + '\"' + test_ans + '\"' + ' ' + '\"' + test_out + '\"'
@@ -48,8 +44,7 @@ def runTestCase(test, test_out, test_ans):
         return True
     return False
 
-
-def testSubmission(submission, labTests, test_case_directory):
+def testSubmission(submission, output_case_directory):
     subprocess.call(['unzip', '-o', ''+submission])
 
     # Extract student_id out of zip filename
@@ -62,39 +57,47 @@ def testSubmission(submission, labTests, test_case_directory):
     correctCases = 0
     totalCases = 0
 
-    #running tests on each test directory
-    for test in labTests:
-        
-        test_cases = glob.glob(dirPath + test_case_directory + test + '/*.ans')
+    test_cases = glob.glob(dirPath + output_case_directory + '*.ans')
+    test_cases.sort()
 
-        #accounting for multiple .in files?
-        #dunno if we need this but okay
-        for testCasePath in test_cases:
-            testName = testCasePath[-6:-4]
-            testHeader = testCasePath[:-4]
+    error_dict = build_error_dict("error_msgs.txt")
 
-            out_file = testHeader + out_file_extension
-            ans_file = testHeader + ans_file_extension
+    # Run tests on each ouput directory file
+    for test_case in test_cases:
 
-            print('Currently testing ' + test + ', test case #' + testName)
-            #lab 4 has just one main java file and multiple text files to test.
-            if runTestCase(test, out_file, ans_file) is True:
-                print("SUCCESS!")
-                correctCases += 1
-            else:
-                print("WRONG")
+        test_num = str(int(test_case[-6:-4]))
+        testHeader = test_case[:-4]
 
-            totalCases += 1
+        out_file = testHeader + out_file_extension
+        ans_file = testHeader + ans_file_extension
+
+        print('\nCurrently testing Lab4, test case #' + test_num)
+
+        if runTestCase(test_num, out_file, ans_file) is True:
+            print("SUCCESS!")
+            correctCases += 1
+        else:
+            print("WRONG! Error in: {}".format(error_dict[test_num]))
+
+        totalCases += 1
 
     return student_id, correctCases, totalCases
 
+# Build a dictionary of case numbers to error messages
+def build_error_dict(filename):
+    errors = {}
+    with open(filename) as fh:
+        for line in fh:
+            number, description = line.strip().split('\t')
+            errors[number] = description.strip()
+    return errors
 
 # Iterate on every .zip file
 for currentZip in submissions:
     # Extract file name from path
     name_of_file = os.path.basename(currentZip)
 
-    student_id, correct, total = testSubmission(name_of_file, labTests, test_case_directory)
+    student_id, correct, total = testSubmission(name_of_file, output_case_directory)
 
     # Record grade in the TestResult text file
     gradebook = open('TestResult.txt', 'a')
@@ -105,20 +108,4 @@ for currentZip in submissions:
     FNULL = open(os.devnull, 'w')
     subprocess.call('rm *.java', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
     subprocess.call('rm *.class', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test1/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test2/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test3/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test4/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test5/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test6/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test7/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test8/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test9/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test10/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test11/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test12/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test13/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test14/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test15/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test16/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-    subprocess.call('rm tests/Lab4Test17/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
+    subprocess.call('rm output/*.out', shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
--- README.md
+++ README.md
@@ -3,3 +3,12 @@ To run tests for a lab on students' submissions:
 1. Place all zipped submissions in the 'Lab4Tester' folder.
 
 2. Execute the command python Lab4Test-Script.py.
+
+.
+├── Lab4Test-Script.py      # Testing script
+├── input                   # Input .txt files with commands
+├── output                  # .ans files with standard output
+├── TestResult.txt          # List of NetIDs and scores, updated after running the script
+├── tools                   # Tools and utilities
+├── LICENSE
+└── README.md
